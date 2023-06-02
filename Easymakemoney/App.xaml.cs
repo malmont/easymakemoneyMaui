40192665aa931a4427a17b50c5640473078ca158
tintin <michel.almont@gmail.com>
2023-06-02 16:58:10 -0400

    Decode Token and decode Api

---
+++ Easymakemoney/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace Easymakemoney;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
+
---
+++ Easymakemoney/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace Easymakemoney;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
+
